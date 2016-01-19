56b3a8fbbd3aa5b5414ac78e357080664d024bd8
JelleTed <JelleTed@gmail.com>
2016-01-19 18:18:22 +0100

    Afvink6
    Hele IntelliJ project toegevoegd

---
+++ src/Node.java
@@ -0,0 +1,20 @@
+public class Node {
+
+    private String sequentie;
+
+    public Node (String sequentie) {
+        this.sequentie = sequentie;
+    }
+
+    public String getSequentie() {
+        return sequentie;
+    }
+
+    public String toString() {
+        return "Node " + sequentie;
+    }
+
+    public Boolean equals (Node node) {
+        return sequentie.equals(node.sequentie);
+    }
+}
